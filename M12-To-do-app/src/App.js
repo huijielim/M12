a26fee9cea5cb19909ffb6864a684847e7cb32b2
huijielim <huijielim@gmail.com>
2021-09-12 15:57:59 +0800

    latest edit

--- M12-To-do-app/src/App.js
+++ M12-To-do-app/src/App.js
@@ -1,47 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
-import Task from './Components/Task'
+import { useState } from "react";
+import "./App.css";
+import Task from "./Components/Task";
 
 const tasks = [
   {
     id: 1,
-    description: 'Wash car'
+    description: "Wash car",
   },
   {
     id: 2,
-    description: 'Buy groceries'
+    description: "Buy groceries",
   },
   {
     id: 3,
-    description: 'Pay water bill'
+    description: "Pay water bill",
   },
-]
+  {
+    id: 4,
+    description: "Walk dog",
+  },
+];
 
 function App() {
-  return (
-    // <div>xxx</div> will deisplay error as can only have 1 div element, to resolve, this, would be to wrap all other divs into the main div.
-    // in react also use the term 'className' rather than 'class' in html
-    // also able to use a framgment which is like '< />' 
-    <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
+  const [todos, setTodos] = useState(tasks);
+  const [inputValue, setInputValue] = useState("");
 
-      <h1>Hi from App Component! :)</h1>
-      {tasks.map((task) => <Task taskInfo={task} />)}
-    </div>
+  const handleChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setTodos([...todos, { description: inputValue, id: todos.length + 1 }]);
+    setInputValue("");
+  };
+
+  const handleDelete = (taskId) => {
+    const filteredTodos = todos.filter((todo) => todo.id !== taskId);
+    setTodos(filteredTodos);
+  };
+
+  const handleEdit = (taskId, editedTask) => {
+    const editedArray = todos.map((todo) => {
+      if (todo.id !== taskId) {
+        return todo;
+      } else {
+        return {
+          id: taskId,
+          description: editedTask,
+        };
+      }
+    });
+    setTodos(editedArray);
+  };
+
+  return (
+    <>
+      <div className='navbar'>my Navbar</div>
+      <div className='App'>
+        <h1>My To Do List</h1>
+        <form onSubmit={handleSubmit}>
+          <input
+            type='text'
+            name='task'
+            id='user-input'
+            value={inputValue}
+            onChange={handleChange}
+          />
+          <input type='button' value='Add Task' />
+        </form>
+        <div className='todos-container'>
+          {todos.map((task, index) => (
+            <Task
+              handleEdit={handleEdit}
+              handleDelete={handleDelete}
+              taskInfo={task}
+              key={index}
+            />
+          ))}
+        </div>
+      </div>
+    </>
   );
 }
 
 export default App;
+
+
--- M12-To-do-app/src/Components/Task.js
+++ M12-To-do-app/src/Components/Task.js
@@ -1,9 +1,33 @@
-const Task = ({ taskInfo }) => {
-    return <div>{taskInfo.description}</div>;
+import { useState } from "react";
+
+const Task = ({ taskInfo, handleDelete, handleEdit }) => {
+  const [isEditClicked, setIsEditClicked] = useState(false);
+  const [editedTask, setEditedTask] = useState(taskInfo.description);
+
+  const handleEditClick = () => {
+    setIsEditClicked(!isEditClicked);
+    if (isEditClicked) {
+      handleEdit(taskInfo.id, editedTask);
+    }
+  };
+
+  const handleEditInput = (e) => {
+    setEditedTask(e.target.value);
+  };
+
+  return (
+    <div className='task-card'>
+      {isEditClicked ? (
+        <input onChange={handleEditInput} value={editedTask} />
+      ) : (
+        <div>{taskInfo.description}</div>
+      )}
+      <div className='card-controller'>
+        <div onClick={() => handleDelete(taskInfo.id)}>X</div>
+        <div onClick={handleEditClick}>{isEditClicked ? "Save" : "Edit"}</div>
+      </div>
+    </div>
+  );
 };
 
-// what ever is in () example 'props' is an object passing from parent to child
-// const Task = (props) => {
-//     return <div>{props.taskInfo.description}</div>;
-// };
 export default Task;
